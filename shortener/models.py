5117c9a03ccc0ee018cf013a49f3966407251554
uniqquej <cjy4477@gmail.com>
2023-04-18 17:02:52 +0900

    form crud 완성

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,5 @@
+import string
+import random
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
@@ -11,3 +13,19 @@ class Users(AbstractUser):
     payplan = models.OneToOneField(PayPlan, on_delete=models.DO_NOTHING, null=True)
     full_name = models.CharField(max_length=100, null=True)
  
+class ShortenedUrls(models.Model):
+    class UrlCreatedVia(models.TextChoices):
+        WEBSITE='web'
+        TELEGRAM='telegram'
+
+    def rand_string():
+        str_pool = string.digits + string.ascii_letters
+        return ("".join([random.choice(str_pool) for _ in range(6)])).lower()
+        
+    nick_name = models.CharField(max_length=100)
+    created_by = models.ForeignKey(Users, on_delete=models.CASCADE)
+    target_url = models.CharField(max_length=2000)
+    shortened_url = models.CharField(max_length=6, default=rand_string)
+    created_via = models.CharField(max_length=8, choices=UrlCreatedVia.choices, default=UrlCreatedVia.WEBSITE)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
